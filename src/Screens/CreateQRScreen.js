d8ea2074845290403e07a1cf88491e9fc5958f40
Abhishek Kushwaha <abhishek@Abhisheks-MacBook-Air.local>
2023-05-13 20:31:59 +0530

    createQR screen done

--- src/Screens/CreateQRScreen.js
+++ src/Screens/CreateQRScreen.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 function CreateQRScreen() {
+  const [text, setText] = useState('');
+  const [showQR, setShowQR] = useState(false);
+
+  const handleGenerateQR = () => {
+    setShowQR(true);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Create QR Screen</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter text"
+        value={text}
+        onChangeText={(value) => setText(value)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleGenerateQR}>
+        <Text style={styles.buttonText}>Generate QR</Text>
+      </TouchableOpacity>
+      {showQR && text !== '' && <QRCode value={text} size={200} />}
     </View>
   );
 }
@@ -14,9 +31,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
-  text: {
-    fontSize: 20,
+  textInput: {
+    borderWidth: 2,
+    borderColor: '#4d94ff',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+    width: '80%',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  button: {
+    backgroundColor: '#4d94ff',
+    borderRadius: 10,
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
     fontWeight: 'bold',
   },
 });
