74360a70ec0a3381e953a30c3b15062b27c6a245
Abhishek Kushwaha <abhishek@Abhisheks-MacBook-Air.local>
2023-05-15 22:59:37 +0530

    produc

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import Routes from './src/Navigation/Routes';
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(()=>{
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Routes />
