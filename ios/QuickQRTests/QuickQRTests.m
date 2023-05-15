856d9437d0845c60b36e522554f1a9f494587e61
Abhishek Kushwaha <abhishek@Abhisheks-MacBook-Air.local>
2023-05-15 23:34:03 +0530

    v1 final

--- ios/QuickQRTests/QuickQRTests.m
+++ ios/QuickQRTests/QuickQRTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAppTests : XCTestCase
+@interface QuickQRTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation QuickQRTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
